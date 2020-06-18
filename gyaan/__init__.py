68d9dba068519bd621b1d653665831e76b506ece
ravitejakotipalli <ravitejak125@gmail.com>
2020-06-18 08:38:13 +0000

    delete gyaan app from master

--- gyaan/__init__.py
+++
@@ -1,4 +0,0 @@
-default_app_config = "gyaan.app.GyaanAppConfig" # pylint:disable=invalid-name
-
-EXECUTE_API_TEST_CASE = True
-__version__ = '0.0.1'
--- gyaan/app.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class GyaanAppConfig(AppConfig):
-    name = "gyaan"
-
-    def ready(self):
-        from gyaan import signals # pylint: disable=unused-variable
